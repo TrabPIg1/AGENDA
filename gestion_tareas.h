a897e44bb3ae890a88289a3a0fca292ba86d5f42
AFAC <andresfelipearias93@hotmail.com>
2015-04-29 22:58:13 +0200

    Update gestion_tareas.h

--- gestion_tareas.h
+++ gestion_tareas.h
@@ -1,3 +1,14 @@
+typedef struct tarea{
+char tipo[10];
+int identif_num;
+char descripcion[50];
+char propietario[20];
+int prioridad;
+char estado[15];
+int fecha;
+struct tarea *prox;
+}Tarea;
+Tarea *liberar(Tarea *cabecera);
 int listar_tarea();
 int ver_tarea();
 int eliminar_tarea();
