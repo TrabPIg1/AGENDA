198e09dd02592129863bee977d1689f483570646
AFAC <andresfelipearias93@hotmail.com>
2015-05-04 23:57:50 +0200

    Update pantalla.h

--- pantalla.h
+++ pantalla.h
@@ -1,1 +1,9 @@
+#define Max 80
+void caracteres(int L, char C,int E); 
+int confir(char mens[]);
+int texto(char C, int E, char mens[]);
+void opciones();
+void opciones1();
+void opciones2();
+void opciones3();
 
